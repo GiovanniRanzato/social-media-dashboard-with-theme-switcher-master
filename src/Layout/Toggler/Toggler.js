f418f7d40cc5724ba59ce4378dd7118a8c6f72b2
Giovanni Ranzato <giovanni.ranzato2010@gmail.com>
2020-09-25 10:27:19 +0200

    Correct accesibility issue

--- src/Layout/Toggler/Toggler.js
+++ src/Layout/Toggler/Toggler.js
@@ -6,9 +6,9 @@ const Toggler = (props) => {
     const [checked, setChecked] = useState(true);
     const switchunchecked = checked ?  null : classes.switchunchecked;
     return <div className={classes.container} >
-        <p className={classes.label}>{props.label} </p>
+        <label className={classes.label}>{props.label} </label>
         <Clickable className={classes.switch +" "+switchunchecked} onClick={()=> { setChecked(!checked); props.onClick()}}>
-        <input type={"checkbox"} checked={checked} onChange={()=>null} label={"change theme"}/> 
+        <input type={"checkbox"} checked={checked} onChange={()=>null} label={props.label} aria-label={props.label} title={props.label}/> 
             <span className={classes.slider}></span>
         </Clickable>
     </div>;
